c93d95e8c8f291879b4d4c26b658b333b269b467
mromanova <mromanova@diasoft.ru>
2021-08-02 03:57:59 +0500

    update for hosting

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,7 +2,7 @@
 export const CREATE_PROFILE = '/create_profile'
 export const PROFILE = '/profile'
 export const VIEW_PROFILES = '/view_profiles'
-export const MAIN = '/'
+export const MAIN = '/vstazherstve_front'
 
 //base api
 export const BASE_API_URL = 'http://localhost:8080'
